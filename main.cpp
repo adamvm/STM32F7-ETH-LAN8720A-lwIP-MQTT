3542a96c223468af4830893780e6302226884516
Kamil Szczygiel <distortec@distortec.com>
2019-06-13 13:41:45 +0200

    Add empty main()

---
+++ main.cpp
@@ -0,0 +1,28 @@
+/**
+ * \file
+ * \brief Main code block.
+ *
+ * \author Copyright (C) 2019 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "distortos/ThisThread.hpp"
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global functions
++---------------------------------------------------------------------------------------------------------------------*/
+
+/**
+ * \brief Main code block of application
+ *
+ * \return doesn't return
+ */
+
+int main()
+{
+	while (1)
+		distortos::ThisThread::sleepFor(std::chrono::seconds{1});
+}
